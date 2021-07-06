7a4f81aeef8d721a112a4c974f73cc27f090415c
mike17061999 <57356325+mike17061999@users.noreply.github.com>
2021-07-07 01:05:51 +0200

    nevermind

--- Xitaso_Webapplikation_DB/Controllers/ProjektController.cs
+++ Xitaso_Webapplikation_DB/Controllers/ProjektController.cs
@@ -14,53 +14,53 @@ namespace Xitaso_Webapplikation.Controllers
 {
     public class ProjektController : Controller
     {
-        public Projekt projekt1 = new Projekt();
-        public Projekt projekt2 = new Projekt();
-        public Analyse analyse1 = new Analyse();
-        public Analyse analyse2 = new Analyse();
-        public Analyse analyse3 = new Analyse();
-        List<Projekt> projects;
-        #region createExampleModels definition
+        //public Projekt projekt1 = new Projekt();
+        //public Projekt projekt2 = new Projekt();
+        //public Analyse analyse1 = new Analyse();
+        //public Analyse analyse2 = new Analyse();
+        //public Analyse analyse3 = new Analyse();
+        //List<Projekt> projects;
+        //#region createExampleModels definition
 
-        public void createExampleModels()
-        {
-            analyse1.Id = 1;
-            analyse1.name = "analyse1";
-            analyse1.comment = "Kommentar von Analyse 1";
-            DateTime thisDay = DateTime.Now;
-            analyse1.lastChanged = thisDay;
-            analyse2.name = "analyse2";
-            analyse2.comment = "Kommentar von Analyse 2";
-            analyse3.name = "analyse3";
-
-
-            projekt1.Id = 1;
-            projekt1.name = "Kunde1";
-            projekt1.projectDescription = "Dies ist die Projektbeschreibung von Kunde1";
-            projekt1.analysis = new List<Analyse>
-            {
-                analyse1,
-                analyse2
-            };
-            projekt2.Id = 2;
-            projekt2.name = "Kunde2";
-            projekt2.projectDescription = "Dies ist die Projektbeschreibung von Kunde2";
-            projekt2.analysis = new List<Analyse>
-            {
-                analyse1,
-                analyse2,
-                analyse3
-            };
-
-            projects = new List<Projekt>
-            {
-                projekt1,
-                projekt2
-            };
+        //public void createExampleModels()
+        //{
+        //    analyse1.Id = 1;
+        //    analyse1.name = "analyse1";
+        //    analyse1.comment = "Kommentar von Analyse 1";
+        //    DateTime thisDay = DateTime.Now;
+        //    analyse1.lastChanged = thisDay;
+        //    analyse2.name = "analyse2";
+        //    analyse2.comment = "Kommentar von Analyse 2";
+        //    analyse3.name = "analyse3";
 
 
-        }
-        #endregion
+        //    projekt1.Id = 1;
+        //    projekt1.name = "Kunde1";
+        //    projekt1.projectDescription = "Dies ist die Projektbeschreibung von Kunde1";
+        //    projekt1.analysis = new List<Analyse>
+        //    {
+        //        analyse1,
+        //        analyse2
+        //    };
+        //    projekt2.Id = 2;
+        //    projekt2.name = "Kunde2";
+        //    projekt2.projectDescription = "Dies ist die Projektbeschreibung von Kunde2";
+        //    projekt2.analysis = new List<Analyse>
+        //    {
+        //        analyse1,
+        //        analyse2,
+        //        analyse3
+        //    };
+
+        //    projects = new List<Projekt>
+        //    {
+        //        projekt1,
+        //        projekt2
+        //    };
+
+
+        //}
+        //#endregion
         private readonly ApplicationDbContext _db;
 
         public ProjektController(ApplicationDbContext db)
@@ -71,9 +71,9 @@ namespace Xitaso_Webapplikation.Controllers
 
         public IActionResult Index()
         {
-            createExampleModels();
+            
             IEnumerable<Projekt> objList = _db.Projekte;
-            return View(new ProjektDashboardViewModel(projects));
+            return View(objList);
         }
 
         public IActionResult Edit()
