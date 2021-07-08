2884caf40853a14058ef87c974d1f9b2f84ec8bf
koppelpe <peter.koppelstetter@student.uni-augsburg.de>
2021-07-08 07:41:35 +0200

    update Display Analyse improved (not working properly)

--- Xitaso_Webapplikation_DB/Controllers/AnalyseController.cs
+++ Xitaso_Webapplikation_DB/Controllers/AnalyseController.cs
@@ -111,7 +111,7 @@ namespace Xitaso_Webapplikation_DB.Controllers
             analyse.analysekategories = list;
             foreach (Analysekategorie analysiscategory in analyse.analysekategories)
             {
-                var list2 = _db.Fragen.Where(a => analysiscategory.Id == a.Id).ToList();
+                var list2 = _db.Fragen.Where(a => analysiscategory.Id == a.analyseKategorieId).ToList();
                 analysiscategory.questions = new List<Frage>(list2);
             }
 
@@ -132,11 +132,20 @@ namespace Xitaso_Webapplikation_DB.Controllers
             return View(new CreateAnalyseViewModel(projectList));
         }
         [HttpPost]
-        public ActionResult Post(Analyse anlyse)
+        public ActionResult Post(Analyse analyse1)
         {
             //Code um neue Analyse in die Datenbank einzufügen
-            analyse1.name = anlyse.name;
-            return RedirectToAction("Index", "Analyse");
+            _db.Analysen.Update(analyse1);
+            foreach (Analysekategorie c in analyse1.analysekategories)
+            {
+                _db.Analysekategorien.Update(c);
+                foreach (Frage f in c.questions)
+                {
+                    _db.Fragen.Update(f);
+                }
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Analyse", new { id = analyse1.Id});
 
         }
 
@@ -223,25 +232,37 @@ namespace Xitaso_Webapplikation_DB.Controllers
             analysekategorie8 = _db.Analysekategorien.Where(a => a.analyseId == analyse.Id && a.name == "Erwartungskonformität").FirstOrDefault();
             Frage frage1 = new Frage();
             frage1.name = "Das System erleichtert die Orientierung durch eine einheitliche Gestaltung";
+            frage1.istBewertung = 1;
+            frage1.SollBewertung = 1;
             frage1.analyseKategorieId = analysekategorie8.Id;
             Frage frage2 = new Frage();
             frage2.name = "Das System lässt den Nutzer nicht im Unklaren darüber, ob eine Eingabe erfolgreich war oder nicht";
+            frage2.istBewertung = 1;
+            frage2.SollBewertung = 1;
             frage2.analyseKategorieId = analysekategorie8.Id;
             Analysekategorie analysekategorie9 = new Analysekategorie();
             analysekategorie9 = _db.Analysekategorien.Where(a => a.analyseId == analyse.Id && a.name == "Individualisierbarkeit").FirstOrDefault();
             Frage frage3 = new Frage();
             frage3.name = "Das System eignet sich für Anfänger und Experten gleichermaßen, weil es auf den Kenntnisstand des Benutzers angepasst werden kann";
+            frage3.istBewertung = 1;
+            frage3.SollBewertung = 1;
             frage3.analyseKategorieId = analysekategorie9.Id;
             Frage frage4 = new Frage();
             frage4.name = "Das System ist so gestaltet, dass der Benutzer die Ausgabe gut an seine individuellen Bedürfnisse anpassen kann (Bildschirmdarstellung/Accesibility) ";
+            frage4.istBewertung = 1;
+            frage4.SollBewertung = 1;
             frage4.analyseKategorieId = analysekategorie9.Id;
             Analysekategorie analysekategorie10 = new Analysekategorie();
             analysekategorie10 = _db.Analysekategorien.Where(a => a.analyseId == analyse.Id && a.name == "Fehlertoleranz").FirstOrDefault();
             Frage frage5 = new Frage();
             frage5.name = "Das System informiert über fehlerhafte Eingaben";
+            frage5.istBewertung = 1;
+            frage5.SollBewertung = 1;
             frage5.analyseKategorieId = analysekategorie10.Id;
             Frage frage6 = new Frage();
             frage6.name = "Das System liefert gut verständliche Fehlermeldungen";
+            frage6.istBewertung = 1;
+            frage6.SollBewertung = 1;
             frage6.analyseKategorieId = analysekategorie10.Id;
 
             _db.Fragen.Add(frage1);
